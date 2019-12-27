b5ea7622e3b866deb701101ba4aa8d8bec0ee715
Stefan Strauch <stefan@stefans-mbp.resource.campus.njit.edu>
2019-12-27 11:48:46 -0500

    Added animation to landing page

--- nav.js
+++ nav.js
@@ -1,7 +1,27 @@
+// Navbar Hamburger Menu
 toggleNav = () => {
   const toggleButton = document.getElementsByClassName('toggle-button')[0];
   const navLinks = document.getElementsByClassName('navbar-links')[0];
 
   navLinks.classList.toggle('active');
   toggleButton.classList.toggle('active');
+};
+
+let home = document.querySelector('#home');
+
+window.onscroll = () => {
+  let title = document.querySelector('#title');
+  let subtitle = document.querySelector('#subtitle');
+
+  let height = window.innerHeight;
+  let scrollTop =
+    window.pageYOffset !== undefined
+      ? window.pageYOffset
+      : (document.documentElement || document.body.parentNode || document.body)
+          .scrollTop;
+
+  height = height / 2;
+
+  title.style.opacity = (height - scrollTop) / height;
+  subtitle.style.opacity = (height - scrollTop) / height;
 };
